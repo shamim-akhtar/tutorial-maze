41f170039f61e966bbe7ba0480ad24ec6cc30441
Shamim Akhtar <shamim.akhtar@gmail.com>
2024-03-01 23:21:46 +0800

    Removed all demo files.

--- Unity2d/Assets/NewScripts/Room.cs
+++ Unity2d/Assets/NewScripts/Room.cs
@@ -1,53 +1,46 @@
-using GameAI.PathFinding;
-using Procedural;
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
 
-public class Room : MonoBehaviour
+namespace Procedural
 {
-  [SerializeField]
-  GameObject[] walls;
-
-  [SerializeField]
-  SpriteRenderer highlight;
-
-  public Vector2Int Index { get; set; }
-  public bool visited = false;
-  bool[] dirflag =
+  public class Room : MonoBehaviour
   {
+    [SerializeField]
+    GameObject[] walls;
+
+    [SerializeField]
+    SpriteRenderer highlight;
+
+    public Vector2Int Index { get; set; }
+    public bool visited = false;
+    bool[] dirflag =
+    {
       true,
       true,
       true,
       true
     };
 
-  public void SetHighlight(bool flag)
-  {
-    highlight.gameObject.SetActive(flag);
-  }
+    public void SetHighlight(bool flag)
+    {
+      highlight.gameObject.SetActive(flag);
+    }
 
-  public void SetHighColor(Color color)
-  {
-    highlight.color = color;
-  }
+    public void SetHighColor(Color color)
+    {
+      highlight.color = color;
+    }
 
-  public void SetActive(Directions dir, bool flag)
-  {
-    walls[(int)dir].SetActive(flag);
-  }
+    public void SetActive(Directions dir, bool flag)
+    {
+      walls[(int)dir].SetActive(flag);
+    }
 
-  // set direction flag for the cell.
-  // a direction flag shows which 
-  // dircetion the cell opens up to.
-  public void SetDirFlag(
-    Directions dir,
-    bool f)
-  {
-    dirflag[(int)dir] = f;
-    SetActive(dir, f);
-    //onSetDirFlag?.Invoke(Value.x, Value.y, dir, f);
+    public void SetDirFlag(
+      Directions dir,
+      bool f)
+    {
+      dirflag[(int)dir] = f;
+      SetActive(dir, f);
+    }
   }
 }
