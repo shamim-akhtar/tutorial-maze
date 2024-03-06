44afdbcaa31ae501e991514e3f6394848ef3e3a4
Shamim Akhtar <shamim.akhtar@gmail.com>
2024-03-06 18:41:49 +0800

    Update GenerateMaze.cs

--- UnityMazeGen/Assets/Scripts/GenerateMaze.cs
+++ UnityMazeGen/Assets/Scripts/GenerateMaze.cs
@@ -1,10 +1,6 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEditor;
-using UnityEditor.Experimental.GraphView;
-using UnityEditor.PackageManager.Requests;
 using UnityEngine;
 
 public class GenerateMaze : MonoBehaviour
